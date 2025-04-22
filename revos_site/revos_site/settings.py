9dacce6f225dc8a3e14b954658361a2c264e1611
Jeet-1577 <mr.jac5333@gmail.com>
2025-04-22 17:08:48 +0530

    ss

--- revos_site/revos_site/settings.py
+++ revos_site/revos_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e0gnnu)=uuh@5nkk%2b1%*!@95e77=n^fc@^^ud#$o=vy^j@yb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
